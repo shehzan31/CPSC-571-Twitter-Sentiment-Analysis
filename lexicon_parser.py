58975cf5225b14a37591fac838636cc2bec69f3a
shehzan31 <47336141+shehzan31@users.noreply.github.com>
2021-12-14 15:40:58 -0700

    Parsed the emotions into different files

--- lexicon_parser.py
+++ lexicon_parser.py
@@ -1,25 +1,104 @@
-fileToOpen = open("Lexicon_English.csv", 'r')
-negativeFile = open("Negative_Lexicon_English.txt", 'w')
+fileToOpen = open("NRC_Lexicon.csv", 'r')
 positiveFile = open("Positive_Lexicon_English.txt", 'w')
+negativeFile = open("Negative_Lexicon_English.txt", 'w')
+
+angryFile = open("Angry_Lexicon_English.txt", 'w')
+anticipationFile = open("Anticipation_Lexicon_English.txt", 'w')
+disgustFile = open("Disgust_Lexicon_English.txt", 'w')
+fearFile = open("Fear_Lexicon_English.txt", 'w')
+joyFile = open("Joy_Lexicon_English.txt", 'w')
+sadnessFile = open("Sad_Lexicon_English.txt", 'w')
+surpriseFile = open("Surprise_Lexicon_English.txt", 'w')
+
+
 fileToOpen.readline()
 
 for line in fileToOpen:
     line = line.splitlines()
     split = line[0].split(',')
-    if((int(split[1]) == 1) ^ (int(split[2]) == 1)):
-        print(split)
-        data = ''
-        file = ''
-        if(int(split[1]) == 1):
-            data = split[0]
-            file = positiveFile
-        if(int(split[2]) == 1):
-            data = split[0]
-            file = negativeFile
+    data = ''
+    file = ''
+
+    if(int(split[1]) == 1):
+        data = split[0]
+        file = positiveFile
         file.writelines(data)
         file.writelines('\n')
+        data = ''
+        file = ''
+    
+    if(int(split[2]) == 1):
+        data = split[0]
+        file = negativeFile
+        file.writelines(data)
+        file.writelines('\n')
+        data = ''
+        file = ''
 
+    if(int(split[3]) == 1):
+        data = split[0]
+        file = angryFile
+        file.writelines(data)
+        file.writelines('\n')
+        data = ''
+        file = ''
+
+    if(int(split[4]) == 1):
+        data = split[0]
+        file = anticipationFile
+        file.writelines(data)
+        file.writelines('\n')
+        data = ''
+        file = ''
+
+    if(int(split[5]) == 1):
+        data = split[0]
+        file = disgustFile
+        file.writelines(data)
+        file.writelines('\n')
+        data = ''
+        file = ''
+
+    if(int(split[6]) == 1):
+        data = split[0]
+        file = fearFile
+        file.writelines(data)
+        file.writelines('\n')
+        data = ''
+        file = ''
+
+    if(int(split[7]) == 1):
+        data = split[0]
+        file = joyFile
+        file.writelines(data)
+        file.writelines('\n')
+        data = ''
+        file = ''
+
+    if(int(split[8]) == 1):
+        data = split[0]
+        file = sadnessFile
+        file.writelines(data)
+        file.writelines('\n')
+        data = ''
+        file = ''
+
+    if(int(split[9]) == 1):
+        data = split[0]
+        file = surpriseFile
+        file.writelines(data)
+        file.writelines('\n')
+        data = ''
+        file = ''
 
 fileToOpen.close()
 negativeFile.close()
 positiveFile.close()
+angryFile.close()
+anticipationFile.close()
+disgustFile.close()
+fearFile.close()
+joyFile.close()
+sadnessFile.close()
+surpriseFile.close()
+
