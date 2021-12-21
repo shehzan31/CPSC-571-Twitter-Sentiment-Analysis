e2d8396a8f025ed1206a3ff58b4b3485b1ef6c2b
adarshakanel <52978035+adarshakanel@users.noreply.github.com>
2021-12-20 20:51:59 -0700

    add emotion comparison

--- compare.py
+++ compare.py
@@ -1,6 +1,7 @@
 import re
 
-nonAutomateEmotionFile = open("parsed_data_short.txt", 'r')
+nonAutomateEmotionFile = open("data_short_new_emotions.csv", 'r')
+nonAutomateEmotionFile.readline()
 automateEmotionFile = open("New_Tweet_File.txt", 'r')
 index = 0
 
@@ -8,41 +9,47 @@ successfulLexiconAndSentimentAnalysis = 0
 successfulSentimentAnalysis = 0
 successfulLexiconEmotionAnalysis = 0
 successfulEmoticonEmotionAnalysis = 0
-
+noEmotion = 0
 sentimentOfEmotion = {
     "fear": ["negative"],
     'disgust': ["negative"],
     "joy": ["positive"],
-    "anticipitation": ["positive", "negative", "neutral"],
+    "anticipation": ["positive", "negative", "neutral"],
     "sad": ['negative'],
     "surprise": ["positive", "negative", "neutral"],
+    "angry": ["negative"],
     "": ["boo"]
 }
 
 while(True):
-    if index < 5:
-        index += 1
-    else:
-        break
+    # if index < 20:
+    index += 1
+    # else:
+    #     break
     nonAutomateLine = nonAutomateEmotionFile.readline()
     automatedLine = automateEmotionFile.readline()
     splitNonAutomatedine = nonAutomateLine.split(',')
     splitAutomatedLine = automatedLine.split(',')
-
-    splitFurtherNonAutomatedine = splitNonAutomatedine[len(
-        splitNonAutomatedine)-1].strip().split(":")
+    if splitAutomatedLine == [''] and splitNonAutomatedine == ['']:
+        break
+    # splitFurtherNonAutomatedine = splitNonAutomatedine[len(
+    #     splitNonAutomatedine)-1].strip().split(":")
     nonAutomatedLineEmotion = re.sub(r'[^\w\s]', '',
-                                     (splitFurtherNonAutomatedine[len(splitFurtherNonAutomatedine)-1].strip()))
+                                     (splitNonAutomatedine[len(splitNonAutomatedine)-1].strip()))
     automatedLineEmoticonEmotion = re.sub(r'[^\w\s]', '',
-                                          (splitAutomatedLine[len(splitAutomatedLine)-1].strip()))
+                                          (splitAutomatedLine[len(splitAutomatedLine)-2].strip()))
     automatedLineLexiconEmotion = re.sub(r'[^\w\s]', '',
-                                         (splitAutomatedLine[len(splitAutomatedLine)-2].strip()))
+                                         (splitAutomatedLine[len(splitAutomatedLine)-1].strip()))
+    print("auto: ", splitAutomatedLine)
+    print("non auto: ", splitNonAutomatedine)
     automatedLineSentiment = re.sub(r'[^\w\s]', '',
                                     (splitAutomatedLine[len(splitAutomatedLine)-3].strip()))
     # print(nonAutomatedLineEmotion, sentimentOfEmotion[nonAutomatedLineEmotion.lower(
     # )], automatedLineEmoticonEmotion,
     #     automatedLineLexiconEmotion, automatedLineSentiment)
-    if (nonAutomatedLineEmotion == automatedLineEmoticonEmotion and automatedLineEmoticonEmotion != '') and (nonAutomatedLineEmotion == automatedLineLexiconEmotion and automatedLineLexiconEmotion != ''):
+    if nonAutomatedLineEmotion == '':
+        noEmotion += 1
+    elif (nonAutomatedLineEmotion == automatedLineEmoticonEmotion and automatedLineEmoticonEmotion != '') and (nonAutomatedLineEmotion == automatedLineLexiconEmotion and automatedLineLexiconEmotion != ''):
         successfulLexiconAndSentimentAnalysis += 1
     elif (nonAutomatedLineEmotion == automatedLineEmoticonEmotion and automatedLineEmoticonEmotion != ''):
         # print(nonAutomatedLineEmotion, automatedLineEmoticonEmotion)
@@ -53,10 +60,11 @@ while(True):
     else:
         nonAutomatedLineSentiment = sentimentOfEmotion[nonAutomatedLineEmotion.lower(
         )]
+        print(nonAutomatedLineSentiment, automatedLineSentiment)
         for emotion in nonAutomatedLineSentiment:
             if emotion in automatedLineSentiment:
                 successfulSentimentAnalysis += 1
                 continue
 
-print(successfulLexiconAndSentimentAnalysis, successfulEmoticonEmotionAnalysis,
-      successfulLexiconEmotionAnalysis, successfulSentimentAnalysis)
+print("total tweets= ", index, "\nno emotion= ", noEmotion, "\nsuccess lexicon+emotion emotion= ", successfulLexiconAndSentimentAnalysis, "\nsuccess emoticon emotion= ", successfulEmoticonEmotionAnalysis,
+      "\nsuccess lexicon emotion= ", successfulLexiconEmotionAnalysis, "\nsuccess sentiment analysis= ", successfulSentimentAnalysis)
